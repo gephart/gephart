67a069f3f4844054c0a8d0a3b6c70706cc3bc683
Michal Katuščák <michal@katuscak.cz>
2017-05-28 19:44:53 +0200

    Add register services

--- web/index.php
+++ web/index.php
@@ -5,4 +5,10 @@ use Gephart\Framework\Kernel;
 include_once __DIR__ . "/../vendor/autoload.php";
 
 $kernel = new Kernel();
+
+$kernel->registerServices([
+    \Admin\EventListener\MenuListener::class,
+    \Admin\EventListener\UserListener::class
+]);
+
 $kernel->run();
