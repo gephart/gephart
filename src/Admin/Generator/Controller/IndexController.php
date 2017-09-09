48df86eeaaadb6e78173a7518a99090e4d357ba2
Michal Katuščák <michal@katuscak.cz>
2017-09-09 16:33:19 +0200

    Refaktor controller -> StatusProvider

--- src/Admin/Generator/Controller/IndexController.php
+++ src/Admin/Generator/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace Admin\Generator\Controller;
 use Admin\Generator\Entity\Module;
 use Admin\Generator\Entity\ModuleStatus;
 use Admin\Generator\Repository\ModuleRepository;
+use Admin\Generator\Service\StatusProvider;
 use Admin\Response\BackendTemplateResponse;
 use Gephart\ORM\Connector;
 
@@ -26,19 +27,19 @@ class IndexController
     private $module_repository;
 
     /**
-     * @var Connector
+     * @var StatusProvider
      */
-    private $connector;
+    private $status_provider;
 
     public function __construct(
         BackendTemplateResponse $template_response,
         ModuleRepository $module_repository,
-        Connector $connector
+        StatusProvider $status_provider
     )
     {
         $this->template_response = $template_response;
         $this->module_repository = $module_repository;
-        $this->connector = $connector;
+        $this->status_provider = $status_provider;
     }
 
     /**
@@ -50,80 +51,12 @@ class IndexController
     public function index()
     {
         $modules = $this->module_repository->findBy();
-        $modules_status = $this->getModulesStatus($modules);
+        $modules_status = $this->status_provider->getModulesStatus($modules);
 
         return $this->template_response->template("admin/generator/index.html.twig", [
             "modules" => $modules,
             "modules_status" => $modules_status
         ]);
-    }
-
-    private function getModulesStatus(array $modules): array
-    {
-        $status = [];
-
-        /** @var Module $module */
-        foreach ($modules as $module) {
-            $status[$module->getEntityName()] = $this->getModuleStatus($module);
-        }
-
-        return $status;
-    }
-
-    private function getModuleStatus(Module $module): ModuleStatus
-    {
-        $status = new ModuleStatus();
-
-        $entity_name = $module->getEntityName();
-
-        $this->setStatusByController($status, $entity_name);
-        $this->setStatusByEntity($status, $entity_name);
-        $this->setStatusByRepository($status, $entity_name);
-        $this->setStatusByView($status, $entity_name);
-        $this->setStatusByTable($status, $module);
-
-        return $status;
-    }
-
-    private function setStatusByController(ModuleStatus $status, string $entity_name) {
-        $controller_dir = realpath(__DIR__ . "/../../Controller");
-
-        if (file_exists($controller_dir . "/" . $entity_name . "Controller.php")) {
-            $status->setController(true);
-        }
-    }
-
-    private function setStatusByEntity(ModuleStatus $status, string $entity_name) {
-        $entity_dir = realpath(__DIR__ . "/../../../App/Entity");
-
-        if (file_exists($entity_dir . "/" . $entity_name . ".php")) {
-            $status->setEntity(true);
-        }
-    }
-
-    private function setStatusByRepository(ModuleStatus $status, string $entity_name) {
-        $repository_dir = realpath(__DIR__ . "/../../../App/Repository");
-
-        if (file_exists($repository_dir . "/" . $entity_name . "Repository.php")) {
-            $status->setRepository(true);
-        }
-    }
-
-    private function setStatusByView(ModuleStatus $status, string $entity_name) {
-        $view_dir = realpath(__DIR__ . "/../../../../template/admin/");
-
-        if (file_exists($view_dir . "/" . $entity_name . "/index.html.twig")
-            && file_exists($view_dir . "/" . $entity_name . "/edit.html.twig")) {
-            $status->setView(true);
-        }
-    }
-
-    private function setStatusByTable(ModuleStatus $status, Module $module) {
-        if ($result = $this->connector->getPdo()->query("SHOW TABLES LIKE '".$module->getSlugSingular()."'")) {
-            if ($result->rowCount() == 1) {
-                $status->setTable(true);
-            }
-        }
     }
 
 }
---
+++ src/Admin/Generator/Service/StatusProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Admin\Generator\Service;
+
+use Admin\Generator\Entity\Module;
+use Admin\Generator\Entity\ModuleStatus;
+use Gephart\ORM\Connector;
+
+final class StatusProvider
+{
+    /**
+     * @var Connector
+     */
+    private $connector;
+
+    public function __construct(
+        Connector $connector
+    )
+    {
+        $this->connector = $connector;
+    }
+
+    public function getModulesStatus(array $modules): array
+    {
+        $status = [];
+
+        /** @var Module $module */
+        foreach ($modules as $module) {
+            $status[$module->getEntityName()] = $this->getModuleStatus($module);
+        }
+
+        return $status;
+    }
+
+    public function getModuleStatus(Module $module): ModuleStatus
+    {
+        $status = new ModuleStatus();
+
+        $entity_name = $module->getEntityName();
+
+        $this->setStatusByController($status, $entity_name);
+        $this->setStatusByEntity($status, $entity_name);
+        $this->setStatusByRepository($status, $entity_name);
+        $this->setStatusByView($status, $entity_name);
+        $this->setStatusByTable($status, $module);
+
+        return $status;
+    }
+
+    private function setStatusByController(ModuleStatus $status, string $entity_name) {
+        $controller_dir = realpath(__DIR__ . "/../../Controller");
+
+        if (file_exists($controller_dir . "/" . $entity_name . "Controller.php")) {
+            $status->setController(true);
+        }
+    }
+
+    private function setStatusByEntity(ModuleStatus $status, string $entity_name) {
+        $entity_dir = realpath(__DIR__ . "/../../../App/Entity");
+
+        if (file_exists($entity_dir . "/" . $entity_name . ".php")) {
+            $status->setEntity(true);
+        }
+    }
+
+    private function setStatusByRepository(ModuleStatus $status, string $entity_name) {
+        $repository_dir = realpath(__DIR__ . "/../../../App/Repository");
+
+        if (file_exists($repository_dir . "/" . $entity_name . "Repository.php")) {
+            $status->setRepository(true);
+        }
+    }
+
+    private function setStatusByView(ModuleStatus $status, string $entity_name) {
+        $view_dir = realpath(__DIR__ . "/../../../../template/admin/");
+
+        if (file_exists($view_dir . "/" . $entity_name . "/index.html.twig")
+            && file_exists($view_dir . "/" . $entity_name . "/edit.html.twig")) {
+            $status->setView(true);
+        }
+    }
+
+    private function setStatusByTable(ModuleStatus $status, Module $module) {
+        if ($result = $this->connector->getPdo()->query("SHOW TABLES LIKE '".$module->getSlugSingular()."'")) {
+            if ($result->rowCount() == 1) {
+                $status->setTable(true);
+            }
+        }
+    }
+}
